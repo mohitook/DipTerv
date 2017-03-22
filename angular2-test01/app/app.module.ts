325cce8bebedc81cd296233cf6548bf51f109690
Nemeth Zsolt <Nemeth Zsolt>
2017-03-22 18:49:59 +0100

    firebase installed

--- angular2-test01/app/app.module.ts
+++ angular2-test01/app/app.module.ts
@@ -3,9 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import {AF} from "./providers/af";
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyA5F2tA1GRgsyJNReTuxuyQC20WrpDRSy4",
+  authDomain: "dipterv-f7bce.firebaseapp.com",
+  databaseURL: "https://dipterv-f7bce.firebaseio.com",
+  storageBucket: "dipterv-f7bce.appspot.com",
+  messagingSenderId: "920667940828"
+};
+
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)],
     declarations: [AppComponent],
+    providers: [AF],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
